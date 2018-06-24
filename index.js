8ed2dc40a71ce9e6fd808772593229fbf33d5682
Adrian Navarrete <a.navarrete.uk@gmail.com>
2018-06-24 17:07:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyarray = [] ; 
+  for(let i = 0; i < musicians.length; i++) {
+   emptyarray.push(`${musicians[i]} plays ${instruments[i]}`) 
+  }
+  return emptyarray
+}
+
+function johnLennonFacts(facts) {
+  var efacts =[] ;
+  let i = 0
+  while (i < facts.length) {
+    efacts.push(`${facts[i]}!!!`)
+    i++;
+  }
+  return efacts
+}
